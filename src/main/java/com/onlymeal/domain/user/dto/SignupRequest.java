150f1fd57aad60bfc02e10d2a6e10bdf63096e0b
HiTae18 <somu950108@gmail.com>
2025-12-10 11:54:16 +0900

    chore: Validation 의존성 및 DTO 검증 추가

--- src/main/java/com/onlymeal/domain/user/dto/SignupRequest.java
+++ src/main/java/com/onlymeal/domain/user/dto/SignupRequest.java
@@ -1,5 +1,9 @@
 package com.onlymeal.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,13 +12,33 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SignupRequest {
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "이메일 형식이 올바르지 않습니다")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다")
     private String password;
+
+    @NotBlank(message = "닉네임은 필수입니다")
     private String nickname;
+
+    @NotBlank(message = "성별은 필수입니다")
     private String gender;
+
+    @NotBlank(message = "생년월일은 필수입니다")
     private String birthDate;
+
+    @NotNull(message = "키는 필수입니다")
+    @Positive(message = "키는 양수여야 합니다")
     private Double height;
+
+    @NotNull(message = "몸무게는 필수입니다")
+    @Positive(message = "몸무게는 양수여야 합니다")
     private Double weight;
+
+    @NotBlank(message = "활동량은 필수입니다")
     private String activityLevel;
+
+    @NotBlank(message = "목표는 필수입니다")
     private String targetGoal;
 }
--- src/main/java/com/onlymeal/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/onlymeal/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,22 @@
 package com.onlymeal.global.exception;
 
 import com.onlymeal.global.common.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.failure("INVALID_INPUT", message));
+    }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
